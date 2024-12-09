7d379d925add91584db3f36667d7a45919db5ebd
Aaron Murillon <abreeze311@gmail.com>
2024-12-09 13:06:50 +0800

    changes from chustomStyle.css and index.html. Added new file scripts.js

---
+++ src/scripts.js
@@ -0,0 +1,16 @@
+$(document).ready(function () {
+    //Menu Sidenav init
+    $('#menuSidenav').sidenav({
+
+    });
+    // Shopping cart Sidenav init
+    $('#shoppingCart').sidenav({
+        edge: 'right'
+    })
+    // Manual trigger desktop shopping cart
+    $('#desktopCart').click(function () {
+        $('#shoppingCart').sidenav('open');
+    });
+
+
+});
